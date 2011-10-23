bbfbab9dc09aafc840266eda436d0fc8a074e4ad
Karel Minarik <karmi@karmi.cz>
2011-10-23 12:52:06 +0200

    Added second implementation, de-duplicating storage of tweets in timelines, using IDs.

--- redis_twitter_example.sh
+++ redis_twitter_example.sh
@@ -195,7 +195,213 @@ ruby -e "
 + "C's timeline:"
 % LRANGE users:C:timeline 0 9
 
-# Of course, we can just as easily display the global timeline.
+# We can just as easily display the global timeline.
 #
 + "Global timeline:"
 % LRANGE global:timeline 0 9
+
+# Of course, we are esentially duplicating the same message in all the user
+# timeline. This way, we would eat out RAM very quickly. How much memory
+# does our “twitter” use now?
+#
++ "Memory usage:"
+% info | \grep "used_memory_human"
+
+# We can de-duplicate the messages by storing them by ID, and storing
+# only those IDs in user timelines, instead of full messages.
+# Let's have a shot at that.
+
+# OK, let's clear everything first.
+#
+% FLUSHDB
+
+# Now, let's again add some users.
+#
++ "Adding users A, B and C"
+% SADD users A B C
+
+# Let's add the relationships:
+#
+# * A follows B and C
+# * C follows B
+# * B does not follow anybody
+#
++ "User A follows user B and C"
+% SADD users:A:following B C
+% SADD users:B:followers A
+% SADD users:C:followers A
++ "User C follows user B"
+% SADD users:C:following B
+% SADD users:B:followers C
+
+# Let B tweet something.
+#
+export message="$(t);Message from B"
+
+# We will store every tweet in under a separate key, with unique ID.
+
+# Let's get a unique, “auto-incrementing” ID, saving it in a `$id` variable.
+#
+export id="$( % INCR tweets:next_id | tr -d " " )"
++ "B publishes message '$message' with ID '$id'"
+
+# Let's store the message content under a key.
+#
+% SET tweets:$id "$message"
+
+# Can we get it back? We can.
+#
+% GET tweets:1
+
+# Now, we have to push the ID to all the timelines, as in the previous implementation.
+
+# The global timeline,
+% LPUSH global:timeline $id
+
+# B's own timeline,
+#
+% LPUSH users:B:timeline $id
+
+# all B's followers timeline.
+#
+% SMEMBERS users:B:followers | \
+ruby -e "
+  followers = STDIN.read.split(\"\n\")
+  id        = ENV['id']
+
+  followers.each do |f|
+    system %Q|redis-cli -n #{ENV['db']} LPUSH users:#{f}:timeline '#{id}'|
+  end
+"
+
+# We should now have the tweet ID stored in relevant timelines.
+
+# Let's have a look on the global one:
+#
++ "Global timeline (IDs):"
+% LRANGE global:timeline 0 -1
+
+# We get back only the IDs. When we want to retrieve the messages itself, we have
+# to fetch them from the relevant keys. It's kinda tricky with the command
+# line client, but doable.
+#
++ "Global timeline (messages):"
+% LRANGE global:timeline 0 -1 | \
+xargs echo | \
+# We will simply replace every numeric ID with the corresponding key in the form `tweets:<ID>`...
+sed 's/\([0-9]\)/tweets:&/g' | \
+# ... and feed it to the `redis-cli`'s [`MGET`](http://redis.io/commands/mget) command.
+xargs redis-cli -n $db MGET
+
+
+# Now, let C tweet something, again.
+
+# Let's get some ID, again
+#
+export id="$( % INCR tweets:next_id | tr -d " " )"
+
+# Let's store the message content under a key, again.
+#
+export message="$(t);Message from C"
++ "C publishes message '$message' with ID '$id'"
+% SET tweets:$id "$message"
+
+# Now, we have to push the ID to all the timelines, again.
+
+# The global timeline,
+% LPUSH global:timeline $id
+
+# C's own timeline,
+#
+% LPUSH users:C:timeline $id
+
+# all C's followers timeline.
+#
+% SMEMBERS users:C:followers | \
+ruby -e "
+  followers = STDIN.read.split(\"\n\")
+  id        = ENV['id']
+
+  followers.each do |f|
+    system %Q|redis-cli -n #{ENV['db']} LPUSH users:#{f}:timeline '#{id}'|
+  end
+"
+
+# And finally, let's repeat everything for A as well.
+#
+export id="$( % INCR tweets:next_id | tr -d " " )"
+export message="$(t);Message from A"
++ "A publishes message '$message' with ID '$id'"
+% SET tweets:$id "$message"
+
+% LPUSH global:timeline $id
+
+% LPUSH users:A:timeline $id
+
+% SMEMBERS users:A:followers | \
+ruby -e "
+  followers = STDIN.read.split(\"\n\")
+  id        = ENV['id']
+
+  followers.each do |f|
+    system %Q|redis-cli -n #{ENV['db']} LPUSH users:#{f}:timeline '#{id}'|
+  end
+"
+
+# Now would be the good time to display the timelines, again.
+
+# Note, that we cannot simply pull messages from the timelines,
+# since we are storing only IDs. We have to feed the fetched
+# IDs to `MGET` again.
+
+# A's timeline.
+#
++ "A's timeline:"
+% LRANGE users:A:timeline 0 9 | xargs echo | \
+  sed 's/\([0-9]\)/tweets:&/g' | \
+  xargs redis-cli -n $db MGET
+
+# B's timeline.
+#
++ "B's timeline:"
+% LRANGE users:B:timeline 0 9 | xargs echo | \
+  sed 's/\([0-9]\)/tweets:&/g' | \
+  xargs redis-cli -n $db MGET
+
+# C's timeline.
+#
++ "C's timeline:"
+% LRANGE users:C:timeline 0 9 | xargs echo | \
+  sed 's/\([0-9]\)/tweets:&/g' | \
+  xargs redis-cli -n $db MGET
+
+# Global timeline.
++ "Global timeline:"
+% LRANGE global:timeline 0 9 | xargs echo | \
+  sed 's/\([0-9]\)/tweets:&/g' | \
+  xargs redis-cli -n $db MGET
+
+# How does our RAM usage look now? You can see it's actually _larger_ then
+# in the previous case, most probably because we're using a larger pool
+# of keys now. There's no free lunch in computer science.
+#
++ "Memory usage:"
+% info | \grep "used_memory_human"
+
+# You may wonder, now, how we display the count of tweets for specific user,
+# for instance. Actually, there's no way to do that.
+#
+# One solution would be to continue with the “query-based” schema, and
+# just keep track of counts manually, in a counter such as `users:A:tweets:count`.
+#
+# Another solution would be to use [_sorted sets_](http://redis.io/commands#sorted_set)
+# for tweets, with one set per user, using timestamp as the score.
+#
+# But that would shift the perspective to “data-based” schema, and we would have to walk
+# through all the sets of all the followers when displaying user's timeline, using
+# [`ZREVRANGEBYSCORE`](http://redis.io/commands/zrevrangebyscore) to get a limited set of IDs,
+# and then perform a [`ZUNIONSTORE`](http://redis.io/commands/zunionstore)
+# to store them in an easily accessible set, sorted by timestamp.
+#
+# And that would certainly be a _very_ expensive set of operations.
+#
