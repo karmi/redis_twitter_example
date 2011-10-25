50c3d44693a3c8727be90343815096142dd64461
Karel Minarik <karmi@karmi.cz>
2011-10-25 09:53:28 +0200

    Overall edits and cleanups to the tutorial code and prose

--- redis_twitter_example.sh
+++ redis_twitter_example.sh
@@ -6,10 +6,11 @@
 
 # This file contains a tutorial explaining core concepts of the [_Redis_](http://redis.io/)
 # database, dressed as a fully working bash script. All the words in UPPERCASE are _Redis_
-# built-in commands. All the words in lowercase are parameters for those commands.
+# built-in commands. All the words in lowercase are parameters for those commands. The
+# `redis-cli` binary is aliased as `%` throughout the script.
 
-# The tutorial implements a simplified _Twitter_ clone, loosely based on the now classic
-# [_TwitterAlikeExample_](http://redis.io/topics/twitter-clone)
+# The tutorial implements a much simplified _Twitter_ “clone”, loosely based on the
+# now classic [_TwitterAlikeExample_](http://redis.io/topics/twitter-clone)
 # by [`@antirez`](http://twitter.com/antirez).
 
 # You may copy and paste snippets from this file, or run it directly:
@@ -18,16 +19,29 @@
 #
 
 # <img src="http://github.com/favicon.ico" style="position:relative; top:2px">
-# The full source for this tutorial is available at <http://github.com/karmi/redis_twitter_example>.
+# The full source code for this tutorial is available at
+# <http://github.com/karmi/redis_twitter_example>.
 
 # ---------------------------------------------------------------------------------------
 
-# First, let's create some aliases for better code readability.
+# First, let's create some aliases for better code readability:
 #
 shopt    -s expand_aliases
+
+# to get current time,
+#
 alias    t="date +%H:%M"
+
+# to define the _Redis_ database used,
+#
 export   db="13"
+
+# to simplify using the `redis-cli` command,
+#
 alias    %="redis-cli -n $db"
+
+# and to simplify displaying notices in the terminal.
+#
 function  + () { echo; echo -e "# \033[1m$@\033[0m"; for i in {1..60}; do echo -n '‾'; done; echo; }
 
 # Second, let's wipe the selected _Redis_ database clean.
@@ -40,9 +54,9 @@ function  + () { echo; echo -e "# \033[1m$@\033[0m"; for i in {1..60}; do echo -
 % SADD users A
 % SADD users B
 
-# Let's make user A follow user B. We will use a _set_ for this, again.
-# Notice the first place where we denormalize the data, storing both
-# sides of the relationship in discrete sets.
+# Let's make user A follow user B. We will use a _set_ for storing the relationship,
+# again. Notice it's the first place where we denormalize the data, storing both
+# sides of the relationship in separate sets.
 #
 + "User A follows user B"
 % SADD users:A:following B
@@ -81,31 +95,46 @@ function  + () { echo; echo -e "# \033[1m$@\033[0m"; for i in {1..60}; do echo -
 + "Display C's followers"
 % SMEMBERS users:C:followers
 
-# OK, it's time for B to tweet something.
+# It's quite easy to display statistics such as “which users follow A and C”
+# in common:
+#
++ "Who is followed by both A and C?"
+% SINTER users:A:following users:C:following
+
+# or display those of C's followers who are not being followed back:
+#
++ "Who is not followed back by C?"
+% SDIFF users:C:followers users:C:following
+
+# OK, it's time for B to tweet something. We'll be storing the message in
+# a corresponding variable.
 #
 # We will store the published time and message body directly in the message
-# itself. In real world, we would just use JSON.
+# itself. (In real world, we would most probably use JSON and not some crazy
+# custom serialization format, obviously.)
 #
 message="$(t);Message from B"
 + "B publishes message '$message'"
 
-
 # We will see how “query-needs” based schema, often called “denormalization”
-# in the RDBMS world really play here. We're optimizing for the maximum **read
-# performance**.
+# in the RDBMS world, really plays here.
+#
+# We're optimizing for the maximum **read performance**.
 
 # First, we have to push the message to the global timeline, possibly
 # displayed on the “twitter” homepage. We will use a Redis _list_
-# for storing the tweets.
+# for storing the tweets: that will allow us to efficiently get
+# parts of the list, trim its size when needed, and the messages
+# will be automatically ordered by the time of publication.
 #
 % LPUSH global:timeline "$message"
 
-# Second, we will push the message to the B's own messages list.
+# Second, we will push the message to the B's own timeline.
 #
 % LPUSH users:B:timeline "$message"
 
 # And, most importantly, third, we have to push the message into
-# the “inbox” of every user following B, which is A and C in our case.
+# the timleine, or “inbox” of every user following B, which is A and C in our case.
 # This will be a bit more tricky.
 
 # First, we have to get a list of all followers of B.
@@ -113,10 +142,9 @@ message="$(t);Message from B"
 % SMEMBERS users:B:followers | \
 
 # Then, we have to iterate over this list, and push B's message
-# into the timeline of each user.
+# into the timeline of each relevant user.
 #
-while read u
-do
+while read u; do
   % LPUSH users:$u:timeline "$message"
 done
 
@@ -125,7 +153,7 @@ done
 message="$(t);Message from C"
 + "C publishes message '$message'"
 
-# We have to run through the loop once again.
+# We have to run through the loop once again:
 
 # 1) push the message to the global timeline,
 #
@@ -135,15 +163,14 @@ message="$(t);Message from C"
 #
 % LPUSH users:C:timeline "$message"
 
-# 3) push the message to C's followers timelines
+# 3) and push the message to C's followers timelines
 #
 % SMEMBERS users:C:followers | \
-while read u
-do
+while read u; do
   % LPUSH users:$u:timeline "$message"
 done
 
-# And finally, let A tweet something as well. We know the drill, now.
+# Finally, let A tweet something as well. We know the drill, by now.
 #
 message="$(t);Message from A"
 + "A publishes message '$message'"
@@ -161,16 +188,16 @@ message="$(t);Message from A"
 # 3) all A's followers timeline (empty in this case).
 #
 % SMEMBERS users:A:followers | \
-while read u
-do
+while read u; do
   % LPUSH users:$u:timeline "$message"
 done
 
-# Now would be the good time to display some tweets.
+# Now would be a good time to display some tweets.
 
 # Let's display A's timeline, trimming it to 10 messages.
-# We can see it contains two tweets, from C and B, in reversed
-# order they were published: C's tweet comes first.
+# We can see it contains three tweets: from A himself, C, and B,
+# in the reversed order they were published, effectively
+# making it a [LIFO queue](http://en.wikipedia.org/wiki/LIFO).
 #
 + "A's timeline:"
 % LRANGE users:A:timeline 0 9
@@ -186,13 +213,14 @@ done
 + "C's timeline:"
 % LRANGE users:C:timeline 0 9
 
-# We can just as easily display the global timeline.
+# We can just as easily display the global timeline, trimming it to
+# just the single last tweet from A.
 #
-+ "Global timeline:"
-% LRANGE global:timeline 0 9
++ "Global timeline (last message):"
+% LRANGE global:timeline 0 0
 
 # Of course, we are esentially duplicating the same message in all the user
-# timeline. This way, we would eat out RAM very quickly. How much memory
+# timelines. This way, we would eat out RAM very quickly. How much memory
 # does our “twitter” use now?
 #
 + "Memory usage:"
@@ -202,11 +230,13 @@ done
 # only those IDs in user timelines, instead of full messages.
 # Let's have a shot at that.
 
+# ---------------------------------------------------------------------------------------
+
 # OK, let's clear everything first.
 #
 % FLUSHDB
 
-# Now, let's again add some users.
+# Now, let's add some users, again, this time in batch.
 #
 + "Adding users A, B and C"
 % SADD users A B C
@@ -225,93 +255,103 @@ done
 % SADD users:C:following B
 % SADD users:B:followers C
 
-# Let B tweet something.
+# Let B tweet something, again.
 #
 message="$(t);Message from B"
 
-# We will store every tweet in under a separate key, with unique ID.
+# We will store every tweet under a separate key, with a unique ID.
 
-# Let's get a unique, “auto-incrementing” ID, saving it in a `$id` variable.
+# We'll get a unique, “auto-incrementing” ID from _Redis_,
+# saving it in a `$id` variable.
 #
 id="$( % INCR tweets:next_id )"
 + "B publishes message '$message' with ID '$id'"
 
-# Let's store the message content under a key.
+# Let's store the message content under a separate key, using the ID.
 #
 % SET tweets:$id "$message"
 
-# Can we get it back? We can.
+# Can we get it back? Sure thing.
 #
 % GET tweets:1
 
-# Now, we have to push the ID to all the timelines, as in the previous implementation.
+# Now, we have to push the ID to all the timelines, as in the previous implementation:
 
-# The global timeline,
+# the global timeline,
+#
 % LPUSH global:timeline $id
 
 # B's own timeline,
 #
 % LPUSH users:B:timeline $id
 
-# all B's followers timeline.
+# all the B's followers timelines.
 #
 % SMEMBERS users:B:followers | \
-while read u
-do
+while read u; do
   % LPUSH users:$u:timeline "$id"
 done
 
-# We should now have the tweet ID stored in relevant timelines.
+# We should now have the tweet ID stored in all relevant timelines.
 
-# Let's have a look on the global one:
+# Let's have a look at the global one:
 #
 + "Global timeline (IDs):"
 % LRANGE global:timeline 0 -1
 
 # We get back only the IDs. When we want to retrieve the messages itself, we have
-# to fetch them from the relevant keys. It's kinda tricky with the command
-# line client, but doable.
+# to fetch them from the relevant keys:
 #
 + "Global timeline (messages):"
+#
 # We will simply replace every numeric ID with the corresponding key in the form `tweets:<ID>`...
+#
 tweet_ids=$( % LRANGE global:timeline 0 9 | sed 's/^/tweets:/' )
+#
 # ... and feed it to the [`MGET`](http://redis.io/commands/mget) command.
+#
 % MGET $tweet_ids
 
 # Now, let C tweet something, again.
 
-# Let's get some ID, again
+# We need to get some ID.
 #
 id="$( % INCR tweets:next_id )"
 
-# Let's store the message content under a key, again.
+# We'll store the message content under a separate key, again.
 #
 message="$(t);Message from C"
 + "C publishes message '$message' with ID '$id'"
 % SET tweets:$id "$message"
 
-# Now, we have to push the ID to all the timelines, again.
+# Now, we have to push the ID to all the relevant timelines, again:
 
-# The global timeline,
+# the global timeline,
 % LPUSH global:timeline $id
 
 # C's own timeline,
 #
 % LPUSH users:C:timeline $id
 
-# all C's followers timeline.
+# and all the C's followers timelines.
 #
 % SMEMBERS users:C:followers | \
-while read u
-do
+while read u; do
   % LPUSH users:$u:timeline "$id"
 done
 
-# And finally, let's repeat everything for A as well.
+# And finally, let's repeat everything for A as well:
+#
+# 1. Getting the unique ID
+# 2. Storing the message
+# 3. Pushing it to the global timeline
+# 4. Pushing it to the author's timeline
+# 5. Pushing it to the author followers' timelines
 #
 id="$( % INCR tweets:next_id )"
 message="$(t);Message from A"
 + "A publishes message '$message' with ID '$id'"
+
 % SET tweets:$id "$message"
 
 % LPUSH global:timeline $id
@@ -319,16 +359,15 @@ message="$(t);Message from A"
 % LPUSH users:A:timeline $id
 
 % SMEMBERS users:A:followers | \
-while read u
-do
+while read u;do
   % LPUSH users:$u:timeline "$id"
 done
 
-# Now would be the good time to display the timelines, again.
+# Now would be a good time to display the timelines, again.
 
 # Note, that we cannot simply pull messages from the timelines,
 # since we are storing only IDs. We have to feed the fetched
-# IDs to `MGET` again.
+# IDs to the `MGET` command, as we'va seen a while ago.
 
 # A's timeline.
 #
@@ -345,16 +384,19 @@ done
 + "C's timeline:"
 % MGET $( % LRANGE users:C:timeline 0 9 | sed 's/^/tweets:/' )
 
-# Global timeline.
+# The global timeline.
+#
 + "Global timeline:"
 % MGET $( % LRANGE global:timeline 0 9 | sed 's/^/tweets:/' ) 
 
 # How does our RAM usage look now? You can see it's actually _larger_ then
-# in the previous case, most probably because we're using a larger pool
-# of keys now. There's no free lunch in computer science.
+# in the previous case, most probably because we're using a slightly
+# larger pool of keys now. There's no free lunch in computer science.
 #
 + "Memory usage:"
 % info | 'grep' "used_memory_human"
+
+# ---------------------------------------------------------------------------------------
 
 # You may wonder, now, how we display the count of tweets for specific user,
 # for instance. Actually, there's no way to do that.
